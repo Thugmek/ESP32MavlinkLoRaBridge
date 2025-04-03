70642db4a1bb1d77b6817723c1dfa4b6784c8e85
Michal Tomek <tomekmichalhu@seznam.cz>
2025-04-03 17:37:13 +0200

    Circulat buffer parametric data type

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -1,18 +1,21 @@
 #include "buffer.hpp"
 #include <iostream>
 
-CircularBuffer::CircularBuffer(uint16_t size):
-    size(size), read_i(0), write_i(0), buffer(new uint8_t[size])
+template <typename T>
+CircularBuffer<T>::CircularBuffer(uint16_t size):
+    size(size), read_i(0), write_i(0), buffer(new T[size])
 {
     std::cout << "Size of Circular Buffer is " << size << "\n";
 }
 
-bool CircularBuffer::isEmpty()
+template <typename T>
+bool CircularBuffer<T>::isEmpty()
 {
     return read_i == write_i;
 }
 
-bool CircularBuffer::isFull()
+template <typename T>
+bool CircularBuffer<T>::isFull()
 {
     if (write_i == size-1)
         return read_i == 0;
@@ -20,17 +23,33 @@ bool CircularBuffer::isFull()
         return read_i == write_i + 1;
 }
 
-void CircularBuffer::push(uint8_t data)
+template <typename T>
+uint16_t CircularBuffer<T>::length(){
+    if(write_i >= read_i){
+        return write_i - read_i;
+    }else{
+        return write_i - read_i + size;
+    }
+}
+
+template <typename T>
+void CircularBuffer<T>::push(T data)
 {
+    if(isFull()) return;
     buffer[write_i] = data;
     write_i++;
     if(write_i == size) write_i = 0;
 }
 
-uint8_t CircularBuffer::pop()
+template <typename T>
+T CircularBuffer<T>::pop()
 {
-    uint8_t data = buffer[read_i];
+    if(isEmpty()) return 0;
+    T data = buffer[read_i];
     read_i++;
     if(read_i == size) read_i = 0;
     return data;
 }
+
+// Buffer data types definition
+template class CircularBuffer<uint8_t>;
--- src/buffer.hpp
+++ src/buffer.hpp
@@ -1,9 +1,10 @@
 #include <cstdint>
 
+template <typename T>
 class CircularBuffer {
     private:
         const uint16_t size;
-        uint8_t* buffer;
+        T* buffer;
         uint16_t write_i;
         uint16_t read_i;
     public:
@@ -11,6 +12,6 @@ class CircularBuffer {
         bool isEmpty();
         bool isFull();
         uint16_t length();
-        void push(uint8_t data);
-        uint8_t pop();
+        void push(T data);
+        T pop();
 };
